63a30b35d1ed378a6ae0bc4103c2e3b90009b26d
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-13 21:58:49 -0400

    saving base code

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const app = express();
 const port = 3000;
 
 // Parse JSON bodies
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-// Webhook endpoint
-app.post('/webhook', (req, res) => {
-  console.log('Received GitHub webhook:');
-  console.log(req.body); // Log the entire payload
-  // You can also extract specific information from the payload and log it
-  console.log('Commit message:', req.body.head_commit.message);
-  console.log('Repository:', req.body.repository.name);
-  console.log('Pusher:', req.body.pusher.name);
-  res.sendStatus(200); // Respond to the webhook with a success status
-});
+// // Webhook endpoint
+// app.post('/webhook', (req, res) => {
+//   console.log('Received GitHub webhook:');
+//   console.log(req.body); // Log the entire payload
+//   // You can also extract specific information from the payload and log it
+//   console.log('Commit message:', req.body.head_commit.message);
+//   console.log('Repository:', req.body.repository.name);
+//   console.log('Pusher:', req.body.pusher.name);
+//   res.sendStatus(200); // Respond to the webhook with a success status
+// });
 
 // Start the server
-// app.listen(port, () => {
-//   console.log(`Webhook server is listening at http://localhost:${port}/webhook`);
-// });
+app.listen(port, () => {
+  console.log(`Webhook server is listening`);
+});
