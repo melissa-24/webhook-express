11a571c639c6156aaddb84b68e2e22d0e9ed0e7d
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-13 22:03:37 -0400

    saving base code

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Parse JSON bodies
-// app.use(bodyParser.json());
-
-// // Webhook endpoint
-// app.post('/webhook', (req, res) => {
-//   console.log('Received GitHub webhook:');
-//   console.log(req.body); // Log the entire payload
-//   // You can also extract specific information from the payload and log it
-//   console.log('Commit message:', req.body.head_commit.message);
-//   console.log('Repository:', req.body.repository.name);
-//   console.log('Pusher:', req.body.pusher.name);
-//   res.sendStatus(200); // Respond to the webhook with a success status
-// });
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Webhook server is listening`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
